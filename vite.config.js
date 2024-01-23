e25c5ac169c7768bd7a6f04ac877cbf17706e42a
YanivZalach <yzal2318@gmail.com>
2024-01-23 16:41:27 +0200

    Adding vite config

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // other configurations...
+
+  build: {
+    // Set the correct base URL here
+    base: '/your-app-name/', // Replace 'your-app-name' with the actual path
+  },
+});
+
